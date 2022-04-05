d5cf36899194b7519f079097e661c60ade2f1d67
Adam Kewley <contact@adamkewley.com>
2022-04-05 16:01:50 +0200

    Added zlib

--- GetDependencies.cmake
+++ GetDependencies.cmake
@@ -364,6 +364,45 @@ if(TRUE)
     )
 endif()
 
+# DEPENDENCY: zlib
+#     support for DEFLATE compression (gz, zip, etc. formats)
+if(TRUE)
+    add_library(zlib STATIC
+        # implementation files
+        third_party/zlib/adler32.c
+        third_party/zlib/compress.c
+        third_party/zlib/crc32.c
+        third_party/zlib/deflate.c
+        third_party/zlib/gzclose.c
+        # below: we aren't using gzip files
+        #third_party/zlib/gzlib.c
+        #third_party/zlib/gzread.c
+        #third_party/zlib/gzwrite.c
+        third_party/zlib/inflate.c
+        third_party/zlib/infback.c
+        third_party/zlib/inftrees.c
+        third_party/zlib/inffast.c
+        third_party/zlib/trees.c
+        third_party/zlib/uncompr.c
+        third_party/zlib/zutil.c
+
+        # private headers
+        third_party/zlib/crc32.h
+        third_party/zlib/deflate.h
+        third_party/zlib/gzguts.h
+        third_party/zlib/inffast.h
+        third_party/zlib/inffixed.h
+        third_party/zlib/inflate.h
+        third_party/zlib/inftrees.h
+        third_party/zlib/trees.h
+        third_party/zlib/zutil.h
+
+        # public header
+        third_party/zlib/zlib.h
+    )
+    target_include_directories(zlib PUBLIC third_party/ third_party/zlib)
+endif()
+
 # DEPENDENCY: OpenSim
 #     primary dependency for performing musculoskeletal simulations
 #
